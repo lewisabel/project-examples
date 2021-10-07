4afa40c2fe3292243fe1a0f723ba394f6df293f9
Glitch (frogger-adaptation) <none>
2021-10-07 22:01:53 +0000

    🐬🏨 Updated with Glitch

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# Bouncy Balls
+# Frogger Classic Adaptation
 
-This project was inspired by [a Sony commercial](https://youtu.be/0_bx8bnCoiU). It generates 100 different circles of different shapes and colours that all move at different speed and directions. It's main goal was to practice object-oriented programming concepts.
+This is an adaptation of the classic arcade game "Frogger". It uses the p5.js library as well as collide2d to create a two player experience where one person is the frog trying to cross to the other side and the other creates obstacles (similar to the cars in the arcade game) to try and hit the frog.
 
 ### Techniques
 
+- JavaScript
 - p5.js library
-- Object-oriented programming
+- p5.collide2d.js library
+- Object-Oriented Programming
+
+### Planning to Implement
+
+- Scoring system
+- Use of sprites
+- Deferent types of obstacles available (e.g. power downs)
+- Cool down period to create obstacles (that varies based on who is winning)
--- script.js
+++ script.js
@@ -1,85 +1,129 @@
-/* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random, noStroke,
-          rect, ellipse, stroke, image, text, mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight */
+/* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
+          rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
+          mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
+          keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize, createButton */
 
-let dots;
+let backgroundColour, frogX, frogY, frogR, frogD, frogV, vehicles, initialVehcX, vehcHit, powerDowns;
 
-function setup() {
+function setup(){
   createCanvas(windowWidth - 20, windowHeight - 20);
+  backgroundColour = 2;
+  
   colorMode(HSB, 360, 100, 100);
-  dots = [];
   
-  for(let i = 0; i<100; i++){
-    dots.push(
-      new bouncyDot(
-      random(width),
-      random(height),
-      random(5, 12),
-      random(360),
-      random(0.5, 3),
-      random(0.5, 3)
-      )
-    )
-  }
+  noStroke();
   
-  // use the master velocities to set xVelocity and yVelocity on each dot
-  for (let i = 0; i < dots.length; i++) {
-    const dot = dots[i];
-    dot.xVelocity = dot.baseXvelocity;
-    dot.yVelocity = dot.baseYvelocity;
-  }
+  // initialize frog for player 1
+  frogX = width/2;
+  frogY = height - 20;
+  frogR = 10;
+  frogD = frogR * 2;
+  
+  // initializes player 2 settings
+  vehicles = [];
+  initialVehcX = [-100, width + 100];
+  powerDowns = [];
+  
 }
 
-function draw() {
-  background(220, 0, 80);
-  for (let i = 0; i < dots.length; i++) {
-    const dot = dots[i];
-    
-    // call the move method
-    dot.move();
-    
-    // draw the dot
-    dot.render();
-    
+function draw(){
+  
+  background(backgroundColour);
+  
+  // frog
+  fill(90, 70, 100);
+  ellipse(frogX, frogY, frogD);
+  
+  // cars move
+  for(let i = 0; i < vehicles.length; i++){
+    let vehicle = vehicles[i];
+    vehicle.move();
+    vehicle.render();
+    vehicle.detectVehcCollisions();
   }
+  
 }
 
-class bouncyDot {
-  constructor(x, y, r, color, baseXvelocity, baseYvelocity){
+function keyPressed(){
+  
+  // move the frog
+  if(keyCode == UP_ARROW){
+    frogY -= 20;
+  }else if(keyCode == DOWN_ARROW){
+    frogY += 20;
+  }else if(keyCode == LEFT_ARROW){
+    frogX -= 20;
+  }else if(keyCode == RIGHT_ARROW){
+    frogX += 20;
+  }
+  
+  // create obstacles
+  else if(keyCode == 65){
+    vehicles.push(new vehicle(random(initialVehcX), random(80, height - 80), random(1, 8)));
+  }
+  
+}
+
+function resetFrog(){
+  // sets frog in original position
+  frogX = width/2;
+  frogY = height - 20;
+}
+
+class vehicle{
+  // defines variables for the vehicle class
+  constructor(x, y, v){
     this.x = x;
     this.y = y;
-    this.r = r;
-    this.color = color;
-    this.baseXvelocity = baseXvelocity;
-    this.baseYvelocity = baseYvelocity;
+    this.v = v;
+    this.initialX = x;
   }
   
   move(){
-    // determining directions of movement and detecting if cirlces have reached an edge
-    this.x += this.xVelocity;
-    this.y += this.yVelocity;
-    if (this.x + this.r > width) {
-      this.xVelocity = -1 * this.baseXvelocity;
+    
+    // if the car starts left of the canvas => move right
+    if(this.initialX < 0){
+      if(this.x < width + 100){
+        this.x += this.v;
+      }
     }
-    if (this.x - this.r < 0) {
-      this.xVelocity = this.baseXvelocity;
-    }
-    if (this.y + this.r > height) {
-      this.yVelocity = -1 * this.baseYvelocity;
-    }
-    if (this.y - this.r < 0) {
-      this.yVelocity = this.baseYvelocity;
+    
+    // if the car starts right of the canvas => move left
+    else if(this.initialX > width){
+      if(this.x > -100){
+        this.x += -1 * this.v;
+      }
     }
   }
   
   render(){
-    fill(this.color, 30, 90);
-    noStroke();
-    ellipse(this.x, this.y, this.r * 2);
+
+    // car showing up on the screen
+    rect(this.x, this.y, 40, 30); 
   }
   
+  detectVehcCollisions(){
+    // detecting collisions with vehicles
+    vehcHit = collideRectCircle(this.x, this.y, 40, 30, frogX, frogY, frogD);
+    
+    // when frog gets hit returns to starting position
+    if(vehcHit == true){
+      resetFrog();
+    }
+  }
 }
 
-function mousePressed() {
-  // console log statements only
-  console.log(dots);
+class powerDown{
+  // defines variables for power downs (e.g. make frog velocity slower)
+  constructor(type, x, y){
+    this.type = type;
+    this.x = x;
+    this.y = y;
+  }
+  render(){
+    
+  }
+  detectPowerDownCollision(){
+    
+  }
 }
